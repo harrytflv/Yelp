14a4537cc7f1f9ac22a684e22429809424317334
xuran zhang <xuranzhang75@gmail.com>
2017-05-05 02:50:13 -0700

    cleaned code for .R

--- xuran_2grams.R
+++ xuran_2grams.R
@@ -3,13 +3,13 @@ library(tm)
 # Read data and check dimension
 review_train <- read.csv('yelp_academic_dataset_review_train.csv')
 review_test <- read.csv('yelp_academic_dataset_review_test.csv')
+# combine train data set and test data together
 whole_data <- rbind(review_train[,c(1:6,8:11)], review_test)
 reviews <- as.vector(whole_data$text)
-# dim(whole_data)
-# dim(review_test)
-
 stars <- c(review_train[,7], rep(0,21919))
 
+
+# corpus function git hint from Raj.
 cleanCorpus = function(corpus){
   review_corpus = tm_map(corpus, content_transformer(tolower))
   review_corpus = tm_map(review_corpus, removeNumbers)
@@ -30,36 +30,42 @@ nGram <- function(tokens, num_grams){
   return(vapply(ngrams(tokens, num_grams), paste, "", collapse = " "))
 }
 
-bi_grams <- lapply(splited_review, function(x, y) nGram(x, 2L))
 
+# 2 grams and 3grams
+bi_grams <- lapply(splited_review, function(x, y) nGram(x, 2L))
+# tri_grams <- lapply(splited_review, function(x, y) nGram(x, 3L))
 # Paste bigram together, so we can store the data as DocumentTermMatrix
 removeSpace <- function(token){
   return(sapply(token, function(x)gsub(" ", "",x) ))
 }
 pasted_bi_grams <- lapply(bi_grams, removeSpace)
+# pasted_tri_grams <- lapply(tri_grams, removeSpace)
 pasted_bi_grams_corpus = Corpus(VectorSource(pasted_bi_grams))
+#pasted_tri_grams_corpus = Corpus(VectorSource(pasted_tri_grams))
 bi_grams_dtm = DocumentTermMatrix(pasted_bi_grams_corpus)
-# dim(bi_grams_dtm)
+# tri_grams_dtm = DocumentTermMatrix(pasted_tri_grams_corpus)
+
+
 # Set the frequency threshold to be 0.99, so we can choose proper features to
 # decrease sparsity.
 bi_grams_dtm_9975 = removeSparseTerms(bi_grams_dtm, 0.9975)
-# dim(bi_grams_dtm_9975)
+# tri_grams_dtm_9995 = removeSparseTerms(tri_grams_dtm, 0.9995)
 
 # Create a ngram matrix
 cleaned_review_dtm = data.frame(as.matrix(bi_grams_dtm_9975))
+# cleaned_review_dtm = data.frame(as.matrix(tri_grams_dtm_9995))
+
 cnames = colnames(cleaned_review_dtm)
 X = cbind(cleaned_review_dtm, stars)
 colnames(X) = c(cnames, "review_stars")
 
-# write.csv(X, file = "reviewTrainAndTest.csv")
+write.csv(X, file = "review_3grams_TrainAndTest.csv")
 # Split data into training and testing
 X_train = X[1:116474,]
 X_test = X[116475:138393,]
 
 # Fit linear model
 lin_mod = glm(review_stars~., data = X_train)
-# summary(lin_mod)
-# a <- summary(lin_mod)$coef
 pred = predict(lin_mod, newdata = data.frame(X_test))
 
 yulaoban = function(x){
@@ -70,4 +76,5 @@ pred = sapply(pred, yulaoban)
 xuran_2grams_output = cbind(as.vector(review_test$business_id), pred)
 colnames(xuran_2grams_output) = c("business_id", "stars")
 write.csv(xuran_2grams_output, file = "xuran_2grams_output.csv")
+# write.csv(xuran_2grams_output, file = "xuran_3grams_output.csv")
 
--- xuran_wordbag.R
+++ xuran_wordbag.R
@@ -4,19 +4,12 @@ library(tm)
 # Read data and check dimension
 review_train <- read.csv('yelp_academic_dataset_review_train.csv')
 review_test <- read.csv('yelp_academic_dataset_review_test.csv')
-# dim(review_test)
+# combine train data set and test data together
 whole_data <- rbind(review_train[,c(1:6,8:11)], review_test)
 reviews <- as.vector(whole_data$text)
-# dim(whole_data)
-# dim(review_test)
 stars <- c(review_train[,7], rep(0,21919))
-max(stars)
 
-# Some custom cleaning function we might consider to add
-# f <- function(x, pattern){gsub(pattern, "", x)}
-# custom_transformation <- content_transformer(f)
-# tm_map(review_corpus, custom_transformation, "[[:digit:]]+")
-
+# corpus function git hint from Raj.
 cleanCorpus = function(corpus){
   review_corpus = tm_map(corpus, content_transformer(tolower))
   review_corpus = tm_map(review_corpus, removeNumbers)
@@ -30,51 +23,41 @@ review_corpus <- cleanCorpus(Corpus(VectorSource(reviews)))
 
 # Change type to DocumentTermMatrix
 review_dtm <- DocumentTermMatrix(review_corpus)
-dim(review_dtm) #6679
 
 # Set the frequency threshold to be 0.99, so we can choose proper features to
 # decrease sparsity.
 cleaned_review_dtm <- removeSparseTerms(review_dtm, 0.99)
-dim(cleaned_review_dtm) #759
-# inspect(cleaned_review_dtm_train[1:5,1:10])
-# cleaned_review_dtm_train
-# Do not run the following line if you computer do not have enough memory...
-# I trained a subset of review data of 10000 observations, which also failed.
-# Should use a much smaller subset.
 cleaned_review_dtm = data.frame(as.matrix(cleaned_review_dtm))
 cnames = colnames(cleaned_review_dtm)
-# max(stars)
 
+# combine the clean data with stars
 X = cbind(cleaned_review_dtm, stars)
 colnames(X) = c(cnames, "review_stars")
-# max(X$review_stars)
-# stars == X$review_stars
 
+# write the clean wordbag data to a CSV
 write.csv(X, "wordbagCleanTrainAndTest.csv")
 haha <- read.csv("wordbagCleanTrainAndTest.csv")
-# View(haha)
-# View(haha["review_stars"])
-# min(haha["review_stars"])
-# max(haha["review_stars"])
+
+# split it to two part as REAL TRAIN and REAL TEST 
 X_train = X[1:116474,]
-# dim(X_train)
-
 X_test = X[116475: nrow(X),]
-# dim(X_test)
 
 
+# Use linear model to predict the review test based on 
+# wordbagCleanTrainAndTest.csv
 lin_mod = glm(review_stars~., data = X_train)
-# summary(lin_mod)
 pred =predict(lin_mod, newdata = data.frame(X_test))
+# since the value may not in (1,5), so we need to write a function to void that.
 yulaoban = function(x){
   min(5, max(1, x))
 }
+# apply the function to the pred
 pred = sapply(pred, yulaoban)
 
+# combine the business_id and stars together, then write it out for Kaggle test.
 business_id = as.vector(review_test$business_id)
 wordbag_output = cbind(business_id, pred)
 colnames(wordbag_output) = c("business_id", "stars")
-
 write.csv(wordbag_output,"xuran_wordbag_output.csv")
 
 
